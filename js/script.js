d52626e2f920b1a310a1eaad033f46c6bf50a704
Zuo Yang <zuya@microsoft.com>
2017-12-27 19:22:17 +0800

    layout

--- js/script.js
+++ js/script.js
@@ -1,11 +1,3 @@
-$('.menu .browse')
-  .popup({
-    inline     : true,
-    hoverable  : true,
-    position   : 'bottom left',
-    delay: {
-      show: 300,
-      hide: 800
-    }
-  })
-;
+$("#Glide").glide({
+  type: "carousel"
+});
